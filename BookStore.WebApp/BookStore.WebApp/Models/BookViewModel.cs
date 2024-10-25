b2040d3ad1fe464882f890218f893cb846253bb5
Meltem Ceran <meltem.ceran@bilgeadamboost.com>
2024-10-25 12:54:05 +0300

    BookStore City Page Add-Page Inıt

---
+++ BookStore.WebApp/BookStore.WebApp/Models/BookViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStore.WebApp.Models
+{
+    public class BookViewModel
+    {
+        public string Name { get; set; }
+        public string BookType { get; set; }
+        public string Authors { get; set; }
+        public short PublishedYear { get; set; }
+        public string PublisHouse { get; set; }
+        public short PageNumber { get; set; }
+        public bool IsPublished { get; set; }
+    }
+}
---
+++ BookStore.WebApp/BookStore.WebApp/Models/PublisherViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.WebApp.Models
+{
+    public class PublisherViewModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string CityName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
