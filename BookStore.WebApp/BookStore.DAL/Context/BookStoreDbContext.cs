558cf9681932dbeb7da278435f5dd7ad4fc2ca52
Meltem Ceran <meltem.ceran@bilgeadamboost.com>
2024-10-25 14:01:46 +0300

    BookStore Db connection first init

---
+++ BookStore.WebApp/BookStore.DAL/Context/BookStoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DAL.Context
+{
+    public class BookStoreDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-3HH2461E;Database=BookStoreDb;Trusted_Connection=true;Trustservercertificate=true");
+        }
+    }
+}
